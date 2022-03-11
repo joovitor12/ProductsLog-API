b8c7b3004d829595362bcca7baf4963dc8130b40
Joao Vitor <outlast4321@gmail.com>
2022-03-10 21:44:44 -0300

    validating data with bean validation

--- productslog-api/src/main/java/com/products/productslog/domain/model/Cliente.java
+++ productslog-api/src/main/java/com/products/productslog/domain/model/Cliente.java
@@ -7,6 +7,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Cliente {
@@ -14,10 +17,17 @@ public class Cliente {
 		@GeneratedValue(strategy = GenerationType.IDENTITY)
 		private Long id;
 		
-		@Column(name = "nome")
+		@NotBlank
+		@Size(max = 70)
 		private String nome;
+		
+		@NotBlank
+		@Email
+		@Size(max = 255)
 		private String email;
 		
+		@NotBlank
+		@Size(max = 20)
 		@Column(name = "fone")
 		private String telefone;
 		public Long getId() {
