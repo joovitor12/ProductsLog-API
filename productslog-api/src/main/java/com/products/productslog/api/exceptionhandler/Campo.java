2d99dbd890c45159576a5b1aa4bcbf66f2978d33
Joao Vitor <outlast4321@gmail.com>
2022-03-10 22:13:19 -0300

    Exception Handler implementation

---
+++ productslog-api/src/main/java/com/products/productslog/api/exceptionhandler/Campo.java
@@ -0,0 +1,24 @@
+package com.products.productslog.api.exceptionhandler;
+
+public class Campo {
+	private String nome;
+	private String mensagem;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	public Campo(String nome, String mensagem) {
+		super();
+		this.nome = nome;
+		this.mensagem = mensagem;
+	}
+	
+}
